277f73da61e899e4c188ed5c0dd519dd13007ce7
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-10-21 17:10:51 +0530

    Added codes for getting todos list

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -1,19 +1,25 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController // from this annotation u can tell program this class will be controller of this project, controller is like a entry point.
+@RestController // from this annotation u can tell program this class will be controller of this project, controller is like an entry point.
 public class TodoController {
 
-    private static List<Todo> todos;
+    private static List<Todo> todoList;
 
     public TodoController() {
-        todos = new ArrayList<>();
-        todos.add(new Todo(1,1,"Todo 1", false ));
-        todos.add(new Todo(2,2,"Todo 2", true));
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1,1,"Todo 1", false ));
+        todoList.add(new Todo(2,2,"Todo 2", true));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos() {
+        return todoList;
     }
 
 }
